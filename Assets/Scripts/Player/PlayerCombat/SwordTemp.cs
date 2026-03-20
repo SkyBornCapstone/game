f71782fe90d825e663af920a5b6897bf8cbe8ca4
Taz Larson <tazlarson120@gmail.com>
2026-03-20 14:22:50 -0700

    Updated animation, will add more next monday

--- Assets/Scripts/Player/PlayerCombat/SwordTemp.cs
+++ Assets/Scripts/Player/PlayerCombat/SwordTemp.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class SwordTemp : MonoBehaviour
 {
     public Transform handPosition;
+    public float xRot = 0;
+    public float yRot = 0;
+    public float zRot = 0;
     
     void Update()
     {
         this.transform.position = handPosition.position;
         this.transform.rotation = handPosition.rotation;
-        this.transform.Rotate(0,0,90);
+        this.transform.Rotate(xRot, yRot, zRot);
     }
 }
