c124d9055c80fb8dfc0296314cac7e9964610cdd
Michael Pflaging <23michaelp@gmail.com>
2026-04-02 10:53:44 -0700

    Add logic for sword sound effects, draw and sheathe unassigned in demo scene

--- Assets/Scripts/Items/SwordItem.cs
+++ Assets/Scripts/Items/SwordItem.cs
@@ -1,14 +1,28 @@
-﻿using Interaction;
+using Interaction;
 using player;
 using UnityEngine;
 
 namespace Items
 {
+    [RequireComponent(typeof(AudioSource))]
     public class SwordItem : Grabbable
     {
+        [SerializeField] private AudioClip swingSound;
+        [SerializeField] private AudioClip hitSound;
+
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
         public override void Use()
         {
             Debug.Log("Get sworded");
+            if (_audioSource != null && swingSound != null)
+            {
+                _audioSource.PlayOneShot(swingSound);
+            }
         }
 
         private void OnCollisionEnter(Collision other)
@@ -18,6 +32,10 @@ namespace Items
             if (other.gameObject.TryGetComponent<PlayerHealth>(out var health))
             {
                 health.TakeDamage(10);
+                if (_audioSource != null && hitSound != null)
+                {
+                    _audioSource.PlayOneShot(hitSound);
+                }
             }
         }
     }
--- Assets/Sound/SoundEffects/attribution.md
+++ Assets/Sound/SoundEffects/attribution.md
@@ -3,7 +3,7 @@ So that I can list the source and license for different sounds:
 **sword_hit.wav**
 Sword sound 1.wav by Merrick079 -- https://freesound.org/s/568170/ -- License: Creative Commons 0
 
-**sword_miss.wav**
+**sword_swing.wav**
 FX_015_sword_swing_1.wav by d00121058 -- https://freesound.org/s/390777/ -- License: Creative Commons 0
 
 **sword_draw.wav**
