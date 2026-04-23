4f6ab8af27b9e6f97f4510121af754b439d17b24
Michael Pflaging <23michaelp@gmail.com>
2026-04-23 12:50:19 -0700

    Islands now float up into place when rendered in instead of popping in out of nowhere

--- Assets/Scripts/terrain/FarlandsVisibilityManager.cs
+++ Assets/Scripts/terrain/FarlandsVisibilityManager.cs
@@ -6,12 +6,15 @@ namespace Terrain
     public class FarlandsVisibilityManager : MonoBehaviour
     {
         public float visibilityThresholdRadius = 400f; 
-        public float randomOffsetRange = 100f; // Each island will pop in progressively slower
+        public float randomOffsetRange = 100f; // Each island threshold offset
+        public float fadeDistanceBand = 75f; // Distance over which the island rises
+        public float riseDepth = 250f; // How far below standard elevation it starts
 
         private class IslandData
         {
             public GameObject Island;
             public float RandomThresholdOffset;
+            public float OriginalY;
             public bool IsVisible;
         }
 
@@ -24,11 +27,15 @@ namespace Terrain
             {
                 Island = island,
                 RandomThresholdOffset = Random.Range(0f, randomOffsetRange),
+                OriginalY = island.transform.position.y,
                 IsVisible = false // Force update check to sync correctly
             };
             
             _farlandsIslands.Add(data);
             island.SetActive(false);
+            
+            // Set it to its hidden depth right away
+            island.transform.position = new Vector3(island.transform.position.x, data.OriginalY - riseDepth, island.transform.position.z);
         }
 
         private void Start()
@@ -39,11 +46,7 @@ namespace Terrain
 
         private void Update()
         {
-            // Checking periodically to stagger the pop in calculations
-            if (Time.frameCount % 10 == 0)
-            {
-                UpdateVisibility();
-            }
+            UpdateVisibility();
         }
 
         private void UpdateVisibility()
@@ -56,20 +59,47 @@ namespace Terrain
 
             Vector3 camPos = _mainCamera.transform.position;
             camPos.y = 0; // measure flat 2D distance to center of the world
-            float distSquared = camPos.sqrMagnitude;
-            float currentDist = Mathf.Sqrt(distSquared);
+            float currentDist = camPos.magnitude;
 
             for (int i = 0; i < _farlandsIslands.Count; i++)
             {
                 var data = _farlandsIslands[i];
                 if (data.Island == null) continue;
 
-                bool shouldBeVisible = currentDist > (visibilityThresholdRadius + data.RandomThresholdOffset);
-
-                if (shouldBeVisible != data.IsVisible)
+                float islandBaseThreshold = visibilityThresholdRadius + data.RandomThresholdOffset;
+                float islandFullyVisibleTarget = islandBaseThreshold + fadeDistanceBand;
+                
+                // If it is entirely too far away
+                if (currentDist < islandBaseThreshold)
                 {
-                    data.IsVisible = shouldBeVisible;
-                    data.Island.SetActive(data.IsVisible);
+                    if (data.IsVisible)
+                    {
+                        data.IsVisible = false;
+                        data.Island.SetActive(false);
+                    }
+                }
+                else
+                {
+                    if (!data.IsVisible)
+                    {
+                        data.IsVisible = true;
+                        data.Island.SetActive(true);
+                    }
+
+                    // Calculate depth
+                    float depthLerp = Mathf.InverseLerp(islandBaseThreshold, islandFullyVisibleTarget, currentDist);
+                    // Smoothstep for prettier rise
+                    depthLerp = Mathf.SmoothStep(0f, 1f, depthLerp);
+                    
+                    float targetY = Mathf.Lerp(data.OriginalY - riseDepth, data.OriginalY, depthLerp);
+                    
+                    Vector3 islandPos = data.Island.transform.position;
+                    // Only update if it actually moved significantly to save physics recalculation ticks if fully rested
+                    if (Mathf.Abs(islandPos.y - targetY) > 0.01f)
+                    {
+                        islandPos.y = targetY;
+                        data.Island.transform.position = islandPos;
+                    }
                 }
             }
         }
