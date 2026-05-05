6a4f5470c7d66be9e664fabf111f85313b2cf030
Michael Pflaging <23michaelp@gmail.com>
2026-05-04 17:36:26 -0700

    add sounds script to player prefab

--- Assets/Scripts/Player/PlayerSounds.cs
+++ Assets/Scripts/Player/PlayerSounds.cs
@@ -6,6 +6,12 @@ namespace Player
     [RequireComponent(typeof(AudioSource))]
     public class PlayerSounds : NetworkBehaviour
     {
+        [Header("Sword Sounds")]
+        [SerializeField] private AudioClip swordUnsheatheSound;
+        [SerializeField] private AudioClip swordSheatheSound;
+        [SerializeField] private AudioClip swordSwingSound;
+        [SerializeField] private AudioClip swordHitSound;
+
         private AudioSource _audioSource;
 
         private void Awake()
@@ -16,29 +22,29 @@ namespace Player
         [ObserversRpc(runLocally: true)]
         public void PlaySwordUnsheathe()
         {
-            if (_audioSource == null) return;
-            // TODO: Play sword unsheathe sound
+            if (_audioSource == null || swordUnsheatheSound == null) return;
+            _audioSource.PlayOneShot(swordUnsheatheSound);
         }
 
         [ObserversRpc(runLocally: true)]
         public void PlaySwordSheathe()
         {
-            if (_audioSource == null) return;
-            // TODO: Play sword sheathe sound
+            if (_audioSource == null || swordSheatheSound == null) return;
+            _audioSource.PlayOneShot(swordSheatheSound);
         }
 
         [ObserversRpc(runLocally: true)]
         public void PlaySwordSwing()
         {
-            if (_audioSource == null) return;
-            // TODO: Play sword swing sound
+            if (_audioSource == null || swordSwingSound == null) return;
+            _audioSource.PlayOneShot(swordSwingSound);
         }
 
         [ObserversRpc(runLocally: true)]
         public void PlaySwordHit()
         {
-            if (_audioSource == null) return;
-            // TODO: Play sword hit sound
+            if (_audioSource == null || swordHitSound == null) return;
+            _audioSource.PlayOneShot(swordHitSound);
         }
     }
 }
